c217528bfb74d655ee506d36a04b2024dc7b2679
greatdane-cloud <77516878+greatdane-cloud@users.noreply.github.com>
2024-01-22 14:55:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-updating-projects-in-real-time
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/greatdane-cloud/vitejs-vite-updating-projects-in-real-time)
---
+++ index.js
@@ -0,0 +1,46 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import {
+  getDatabase,
+  ref,
+  push,
+} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+
+const appSettings = {
+  databaseURL:
+    'https://realtime-database-df319-default-rtdb.europe-west1.firebasedatabase.app/',
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, 'shoppingList');
+
+const inputFieldEl = document.getElementById('input-field');
+const addButtonEl = document.getElementById('add-button');
+const shoppingListEl = document.getElementById('shopping-list');
+
+addButtonEl.addEventListener('click', function () {
+  let inputValue = inputFieldEl.value;
+
+  push(shoppingListInDB, inputValue);
+
+  clearInputFieldEl();
+
+  appendItemToShoppingListEl(inputValue);
+});
+
+/*
+Challenge:
+Call the onValue function with
+shoppingListInDB as the first argument and
+function(snapshot) {} as the second argument
+*/
+
+onValue(shoppingListInDB, function (snapshot) {});
+
+function clearInputFieldEl() {
+  inputFieldEl.value = '';
+}
+
+function appendItemToShoppingListEl(itemValue) {
+  shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+}
