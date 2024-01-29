74ebbb732200246e49d05a03b5dda64084bc655d
greatdane-cloud <vanderbyljeandre@gmail.com>
2024-01-29 23:52:26 +0200

    making the pots happen

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
 import {
   getDatabase,
   ref,
   push,
-  onValue
-} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+  onValue,
+  remove,
+} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
 
 const appSettings = {
   databaseURL:
-  "https://playground-5b289-default-rtdb.europe-west1.firebasedatabase.app/",
-};
+    'https://playground-5b289-default-rtdb.europe-west1.firebasedatabase.app/',
+}
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-const shoppingListInDB = ref(database, 'shoppingList');
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, 'shoppingList')
 
-const inputFieldEl = document.getElementById('input-field');
-const addButtonEl = document.getElementById('add-button');
-const shoppingListEl = document.getElementById('shopping-list');
+const inputFieldEl = document.getElementById('input-field')
+const addButtonEl = document.getElementById('add-button')
+const shoppingListEl = document.getElementById('shopping-list')
 
 addButtonEl.addEventListener('click', function () {
-  let inputValue = inputFieldEl.value;
+  let inputValue = inputFieldEl.value
 
-  push(shoppingListInDB, inputValue);
+  push(shoppingListInDB, inputValue)
 
-  clearInputFieldEl();
-
- 
-});
+  clearInputFieldEl()
+})
 
 /*
 Challenge:
@@ -37,36 +36,45 @@ function(snapshot) {} as the second argument
 */
 
 onValue(shoppingListInDB, function (snapshot) {
-  let itemsArray = Object.entries(snapshot.val())
+  // Challenge: Change the onValue code so that it uses snapshot.exists() to show items when there are items in the database and if there are not displays the text 'No items here... yet'.
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val())
 
-  clearShoppingListEl()
+    clearShoppingListEl()
 
-  for (let i = 0; i < itemsArray.length; i++){
-    let currentItem = itemsArray[i]
-    let currentItemID = currentItem[0]
-    let currentItemValue = currentItem[1]
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i]
+      let currentItemID = currentItem[0]
+      let currentItemValue = currentItem[1]
 
-
-    appendItemToShoppingListEl(currentItem)
+      appendItemToShoppingListEl(currentItem)
+    }
+  } else {
+    shoppingListEl.innerHTML = 'No items here... yet'
   }
+})
 
-  });
-
-function clearShoppingListEl(){
-  shoppingListEl.innerHTML = ""
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = ''
 }
 
 function clearInputFieldEl() {
-  inputFieldEl.value = '';
+  inputFieldEl.value = ''
 }
 
 function appendItemToShoppingListEl(item) {
   let itemID = item[0]
   let itemValue = item[1]
 
-  let newEl = document.createElement("li")
+  let newEl = document.createElement('li')
 
   newEl.textContent = itemValue
+
+  newEl.addEventListener('click', function () {
+    let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
+
+    remove(exactLocationOfItemInDB)
+  })
 
   shoppingListEl.append(newEl)
 }
